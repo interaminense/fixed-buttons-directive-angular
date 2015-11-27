f4750c2ff85806cf52d60970b27d3e5c6dbf7690
Adriano Interaminense <adriano.interaminense@gmail.com>
2015-11-27 02:32:55 -0200

    Initial commit, add files of the directive angular

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('app',[]);
+
+app.controller('ctrl', ['$scope', function($scope) {
+  // $scope.teste = function(){
+  //   document.querySelector('.content-wrapper').style.display = 'block';
+  // }
+}]);
+
+app.directive('tgFixedButtons', ['$window', '$document', function ($window, $document) {
+
+  return {
+    restrict: 'E',
+    link: function (scope, elem) {
+
+      scope.functionPositionButtons = function(){
+        if($window.pageYOffset + $window.innerHeight < $document[0].body.clientHeight){
+          elem[0].classList.add("fixed-bottom");
+        }else{
+          elem[0].classList.remove("fixed-bottom");
+        }
+      }
+
+      //calls this function to load the page
+      scope.functionPositionButtons();
+
+      //calls this function by clicking the dom
+      angular.element($document).on('click', function(){
+        scope.functionPositionButtons();
+      });
+
+      //calls this function to scroll the page
+      angular.element($window).on('scroll', function(){
+        scope.functionPositionButtons();
+      });
+
+    }
+  }
+
+}]);
